60cdb6b3d3943bd0bca88766a2a3973fbabc82e7
guogj <295719991@qq.com>
2023-12-05 10:39:04 +0800

    add

--- packages/components/src/icon/icon.vue
+++ packages/components/src/icon/icon.vue
@@ -2,5 +2,6 @@
     <div class="j-icon">icon</div>
 </template>
 <script lang="ts" setup>
+defineOptions({ name: "j-icon" });
 import "./style/index.less";
 </script>
--- play/app.vue
+++ play/app.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
       <j-button type="primary"> 主要按钮</j-button>
+      <j-icon/>
     </div>
   </template>
   <script lang="ts" setup>
-  import { Button } from "@jun-ui/components";
+  // import { Button } from "@jun-ui/components";
   </script>
   
